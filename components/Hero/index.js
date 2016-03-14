7156f73f11d5cd7013e19e823dcebe6cd5a65a5a
ChristopherBiscardi <chris@christopherbiscardi.com>
2016-03-14 01:09:10 -0700

    some small fixups

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -5,10 +5,7 @@ import styles from './Hero.css';
 export default class Nav extends Component {
   render() {
     return (
-      <div className={styles.wrapper}
-      style={{
-        backgroundImage: `url(/${require('./default-bg.jpeg')})`
-      }}>
+      <div className={styles.wrapper}>
         <div className={styles.upper}>
           <h1>Chris Biscardi</h1>
           <p>Haskell, React, Design, Products</p>
