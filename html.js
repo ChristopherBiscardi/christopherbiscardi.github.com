f7eb5b2f0d2c6980b558ade3a2ab59863f7c6e8f
Christopher Biscardi <biscarch@sketcht.com>
2016-05-04 16:23:35 -0700

    remove proxima nova

---
+++ html.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+export default class HTML extends Component {
+
+  render() {
+    const {
+      bundleAssets
+    } = this.props;
+
+    return (
+      <html lang="en">
+      <head>
+        <meta charSet="utf-8"/>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" type="text/css" href={`/${bundleAssets.css}`}/>
+      </head>
+      <body className="landing-page">
+        <div id="react-mount" dangerouslySetInnerHTML={{__html: this.props.body}} />
+      {/* Temporarily remove the client JS
+        <script src={`/${bundleAssets.js}`} />
+      */}
+        <script dangerouslySetInnerHTML={{__html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-46878058-1', 'auto');
+          ga('send', 'pageview');`}}/>
+      </body>
+      </html>
+    )
+  }
+}
