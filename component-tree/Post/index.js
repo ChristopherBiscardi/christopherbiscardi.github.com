f52ab0557d662aae76afa7a202a55e206f067fb8
ChristopherBiscardi <chris@christopherbiscardi.com>
2016-08-03 07:15:38 -0700

    opengraph tags

--- component-tree/Post/index.js
+++ component-tree/Post/index.js
@@ -10,33 +10,19 @@ import styles from './Post.css';
 class PostComponent extends Component {
   render() {
     const {
-      date,
       timeToRead,
       title,
       updatedAt,
       publishedAt,
       headerImage,
       canonicalURL,
-      url
+      url,
+      excerpt
     } = this.props.root.post.attributes;
 
     return (
       <div>
-        <Helmet
-            title={title}
-            meta={[
-              {
-                property: "og-title",
-                content: title
-              }, {
-                property: "og-type",
-                content: "article"
-              }, {
-                property: "og-url",
-                content: __DOMAIN__ + (canonicalURL || url)
-              }
-            ]}
-        />
+        { this.renderHelmet() }
         <div className={styles.container}>
           <div className={styles.singleColumn}>
             <h1 className={styles.title}>{title}</h1>
@@ -56,6 +42,61 @@ class PostComponent extends Component {
       </div>
     )
   }
+  renderHelmet() {
+    const {
+      title,
+      updatedAt,
+      publishedAt,
+      headerImage,
+      canonicalURL,
+      url,
+      excerpt
+    } = this.props.root.post.attributes;
+
+    return (
+      <Helmet
+          title={title}
+          meta={[
+            {
+              property: "description",
+              content: excerpt && excerpt.slice(0,155)
+            }, {
+              property: "og:title",
+              content: title
+            }, {
+              property: "og:type",
+              content: "article"
+            }, {
+              property: "og:url",
+              content: __DOMAIN__ + (canonicalURL || url)
+            }, {
+              property: "og:description",
+              content: excerpt
+            }, {
+              property: "og:image",
+              content: __DOMAIN__ + headerImage
+            }, {
+              property: "twitter:card",
+              content: "summary"
+            }, {
+              property: "twitter:title",
+              content: title.slice(0,70)
+            }, {
+              property: "twitter:description",
+              content: excerpt && excerpt.slice(0,200)
+            }, {
+              property: "twitter:url",
+              content: __DOMAIN__ + (canonicalURL || url)
+            }, /* TODO: enable this after image-plugin resize work
+                  {
+                  property: "twitter:image",
+                  content: __DOMAIN__ + twitterImage
+                  }
+                */
+          ]}
+      />
+    )
+  }
 }
 
 export default Relay.createContainer(PostComponent, {
@@ -71,6 +112,7 @@ export default Relay.createContainer(PostComponent, {
          headerImage,
          url
          canonicalURL
+         excerpt
        }
        body
      }
