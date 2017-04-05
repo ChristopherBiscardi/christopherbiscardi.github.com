5ddd08f83fe4db67f2de647e7b5322999dcff236
Chris Biscardi <chris@christopherbiscardi.com>
2017-04-05 13:39:55 -0700

    ship it?

--- component-tree/Home/index.js
+++ component-tree/Home/index.js
@@ -1,75 +1,83 @@
-import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router-dom';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
-import Hero from '../Hero';
-import PostCard from '../../components/PostCard';
-import styles from './Home.css';
+import React, { Component, PropTypes } from "react";
+import { Link } from "react-router-dom";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import Hero from "../Hero";
+import PostCard from "../../components/PostCard";
+import styles from "./Home.css";
 const { bool, string } = PropTypes;
-import Helmet from 'react-helmet';
-import { css } from 'glamor';
+import Helmet from "react-helmet";
+import { css } from "glamor";
 
 const gStickyWrapper = css({
-  display: 'flex',
-  padding: '1rem 2rem',
-  backgroundColor: '#FAFAFA',
-  color: '#010d13',
-  '@media only screen and (max-width: 480px)': {
-    flexFlow: 'column nowrap'
+  display: "flex",
+  padding: "1rem 2rem",
+  backgroundColor: "#FAFAFA",
+  color: "#010d13",
+  "@media only screen and (max-width: 480px)": {
+    flexFlow: "column nowrap"
   }
 });
 const gPostsWrapper = css({
-  padding: '0 2rem',
-  backgroundColor: '#FAFAFA',
+  padding: "0 2rem",
+  backgroundColor: "#FAFAFA"
 });
 const gPosts = css({
-  lostFlexContainer: 'row',
-  '@media (width >= 1440px)': {
-    padding: '1rem 0',
-    lostCenter: '1440px',
+  label: "gPosts",
+  "@media only screen and (min-width: 800px)": {
+    display: "grid",
+    gridTemplateColumns: "1fr .5fr .5fr 1fr",
+    gridColumnGap: "1rem",
+    gridRowGap: "1rem"
+  },
+  "@media only screen and (min-width: 1440px)": {
+    padding: "1rem 0",
+    lostCenter: "1440px"
   }
 });
 const gStickyInnerWrapper = css({
-  flexFlow: 'row wrap',
-  '@media (width >= 1440px)': {
-    lostCenter: '1440px'
+  flexFlow: "row wrap",
+  "@media (width >= 1440px)": {
+    lostCenter: "1440px"
   }
 });
 
 const gReadMore = css({
-  color: '#0B95E1',
-  fontSize: '0.8rem',
-  margin: '0',
+  color: "#0B95E1",
+  fontSize: "0.8rem",
+  margin: "0"
 });
 
 const gStickyMeta = css({
-  lostColumn: '2/10 2',
-  '@media only screen and (max-width: 480px)': {
-    lostColumn: '1/1 1',
+  lostColumn: "2/10 2",
+  "@media only screen and (max-width: 480px)": {
+    lostColumn: "1/1 1"
   }
 });
 
 const gStickyContent = css({
-  lostColumn: '8/10 2',
-  '@media only screen and (max-width: 480px)': {
-    lostColumn: '1/1 1',
+  lostColumn: "8/10 2",
+  "@media only screen and (max-width: 480px)": {
+    lostColumn: "1/1 1"
   }
 });
 
 const gFeatured = css({
-  textAlign: 'end',
-  marginBottom: '0',
-  color: '#0B95E1',
-  '@media only screen and (max-width: 480px)': {
-    fontWeight: 'bold',
+  textAlign: "end",
+  marginBottom: "0",
+  color: "#0B95E1",
+  "@media only screen and (max-width: 480px)": {
+    fontWeight: "bold"
   }
 });
 
 class StickyComponent extends Component {
   render() {
     const {
-      title, excerpt, url
-    } = this.props.sticky.attributes
+      title,
+      excerpt,
+      url
+    } = this.props.sticky.attributes;
 
     return (
       <Link to={url}>
@@ -89,34 +97,32 @@ class StickyComponent extends Component {
           </div>
         </div>
       </Link>
-    )
+    );
   }
 }
 
 const gPost = css({
+  label: 'gPost',
   display: 'flex',
-  lostColumn: '1/1',
-  '@media (width >= 480px)': {
-    ':first-child': {
-      lostColumn: '1/2 2'
+  gridColumnStart: "span 2",
+  "@media only screen and (min-width: 480px)": {
+    ":nth-child(3)": {
+      gridColumnStart: "span 1"
     },
-    ':nth-child(2)': {
-      lostColumn: '1/2 2'
-    },
-    display: 'flex',
-    lostColumn: '1/3 0',
+    ":nth-child(5)": {
+      gridColumnStart: "span 1"
+    }
   }
 });
 
 export class HomeComponent extends Component {
-
   render() {
     const {
       root,
       loading
     } = this.props.data;
-    if(loading) {
-      return <div>HomeLoading</div>
+    if (loading) {
+      return <div>HomeLoading</div>;
     }
     return (
       <div>
@@ -126,34 +132,32 @@ export class HomeComponent extends Component {
             {
               property: "og:url",
               content: __DOMAIN__
-            }, {
+            },
+            {
               property: "og:description",
               content: "Chris Biscardi builds products for startups using functional programming."
-            }, {
-              property: "og:image",
-              content: __DOMAIN__ + require('../Hero/default-bg.jpeg')
             },
+            {
+              property: "og:image",
+              content: __DOMAIN__ + require("../Hero/default-bg.jpeg")
+            }
           ]}
         />
         <Hero />
-        { root.sticky && <StickyComponent sticky={root.sticky}/> }
+        {root.sticky && <StickyComponent sticky={root.sticky} />}
         <div {...gPostsWrapper}>
           <div {...gPosts}>
-          {
-            root.posts && root.posts.edges.map( ({ node }) => (
-              // lost-column div size here
-              <div
-                {...gPost}
-                key={node.attributes.slug}
-              >
-                <PostCard post={node} />
-              </div>
-            ))
-          }
+            {root.posts &&
+              root.posts.edges.map(({ node }) =>
+              (
+                <div {...gPost} key={node.attributes.slug}>
+                  <PostCard post={node} />
+                </div>
+              ))}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -173,12 +177,12 @@ const Query = gql`query HomeQuery {
   }
 }
 ${PostCard.fragments.post}
-`
+`;
 
 export default graphql(Query, {
   options: ({ params }) => {
     return {
       variables: {}
-    }
-  },
+    };
+  }
 })(HomeComponent);
--- components/PostCard.js
+++ components/PostCard.js
@@ -1,10 +1,72 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component, PropTypes } from "react";
 const { bool, string } = PropTypes;
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
-import { propType } from 'graphql-anywhere';
-import { Link } from 'react-router-dom';
-import styles from './PostCard.css';
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { propType } from "graphql-anywhere";
+import { Link } from "react-router-dom";
+import { css } from "glamor";
+
+const gPost = css({
+  marginBottom: "2rem",
+  padding: "1rem",
+  backgroundColor: "white",
+  borderRadius: "3px",
+  boxShadow: "0 1px 4px rgba(0,0,0,.04)",
+  border: "1px solid rgba(0,0,0,.09)",
+  display: "flex",
+  flex: 1,
+  flexDirection: "column",
+  justifyContent: "space-between",
+  /*
+   Below prevents words that are continuous and longer than the
+   container from overflowing
+  */
+  wordWrap: "break-word",
+  minWidth: 0
+});
+
+const gPostHeader = css({
+  margin: "-1rem",
+  borderTopLeftRadius: "3px",
+  borderTopRightRadius: "3px",
+  // Because percent padding is relative to width, the "height" will be 2/5 of the width.
+  height: 0,
+  paddingBottom: "40%"
+});
+
+const blueprintBackground = css({
+  // Originally from http://lea.verou.me/css3patterns/#blueprint-grid
+  backgroundColor: "#ecf8ff",
+  backgroundImage: `
+linear-gradient(white 2px, transparent 2px),
+linear-gradient(90deg, white 2px, transparent 2px),
+linear-gradient(rgba(255,255,255,.3) 1px, transparent 1px),
+linear-gradient(90deg, rgba(255,255,255,.3) 1px, transparent 1px)
+`,
+  backgroundSize: "100px 100px, 100px 100px, 20px 20px, 20px 20px",
+  backgroundPosition: "-2px -2px, -2px -2px, -1px -1px, -1px -1px"
+});
+
+const gBlueprintHeader = css(blueprintBackground, gPostHeader);
+
+const gHeading = css({
+  label: 'heading',
+  color: "#373a3c",
+  marginBottom: 0
+});
+
+const gMeta = css({
+  fontSize: "0.8rem"
+});
+
+const gReadMore = css({
+  color: "#AEAEAE",
+  fontSize: "0.8rem"
+});
+
+const gExcerpt = css({
+  marginBottom: ".5rem"
+});
 
 class PostCard extends Component {
   static fragments = {
@@ -21,7 +83,7 @@ class PostCard extends Component {
           headerImage
         }
       }
-    `,
+    `
   };
   render() {
     const {
@@ -34,25 +96,35 @@ class PostCard extends Component {
       headerImage
     } = this.props.post.attributes;
 
-    var maybeHeaderImage = <div className={styles.blueprintHeader}></div>;
-    if(headerImage) {
+    var maybeHeaderImage = <div {...gBlueprintHeader} />;
+    if (headerImage) {
       maybeHeaderImage = (
-        <div className={styles.imageHeader}
-             style={{backgroundImage: `url(${headerImage})`}}></div>
+        <div
+          {...css(
+            {
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundImage: `url(${headerImage})`
+            },
+            gPostHeader
+          )}
+        />
       );
     }
 
     return (
-      <div className={styles.post}>
+      <div {...gPost}>
         {maybeHeaderImage}
         <div>
-          <Link to={url}><h4 className={styles.heading}>{title}</h4></Link>
-          <span className={styles.meta}>{updatedAt} &bull; {timeToRead} minute read </span>
+          <Link to={url}><h4 {...gHeading}>{title}</h4></Link>
+          <span {...gMeta}>
+            {updatedAt} • {timeToRead} minute read{" "}
+          </span>
         </div>
-        <p className={styles.excerpt}>{excerpt}</p>
-        <Link to={url} className={styles.readMore}>Read more...</Link>
+        <p {...gExcerpt}>{excerpt}</p>
+        <Link to={url} {...gReadMore}>Read more...</Link>
       </div>
-    )
+    );
   }
 }
 
