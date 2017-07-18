0570300ab320bd5b58ce4c4564ba5dbe8d8ea915
Christopher Biscardi <biscarch@sketcht.com>
2017-07-17 18:00:47 -0700

    rm -rf leo

--- leo-plugins/blogpost-archives/process.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(data, cb) {
-  const blogposts = data.filter(o => o.contentType === 'leo-blogpost');
-
-  cb(null, data);
-}
-
-// TODO: medium-loader
-// Finish Preact? Add another scaffolding plugin
