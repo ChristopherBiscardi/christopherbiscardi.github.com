2f5f112bf83a03d99d9cdbd397d43da5da37a203
Christopher Biscardi <biscarch@sketcht.com>
2019-01-14 23:33:44 -0800

    maybe an rss feed?

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Chris Biscardi's website`
+    title: `Chris Biscardi`,
+    author: "Chris Biscardi",
+    description: "Chris' thoughts and posts",
+    siteUrl: "https://www.christopherbiscardi.com/",
+    social: {
+      twitter: `chrisbiscardi`
+    }
   },
   plugins: [
     {
@@ -27,6 +33,60 @@ module.exports = {
       }
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-offline`
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }]
+                });
+              });
+            },
+            query: `
+            {
+              allMdx(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] }
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: "/rss.xml",
+            title: "Chris Biscardi RSS Feed"
+          }
+        ]
+      }
+    }
   ]
 };
