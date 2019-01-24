807af8a882d765a7ddbfc2bea0bb40557a8f2e10
Christopher Biscardi <chris@christopherbiscardi.com>
2019-01-23 21:32:00 -0800

    Frontpage images (#70)
    * wip
    
    * works-ish

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
+import { graphql, Link } from "gatsby";
 import theme from "@sens8/tokens";
 import { Heading } from "sens8";
+import Img from "gatsby-image";
+
 import Nav from "../navigation";
 
 export default class IndexPage extends Component {
   render() {
+    const { featuredPosts } = this.props.data;
     return (
       <div>
         <Helmet>
@@ -20,9 +24,8 @@ export default class IndexPage extends Component {
             background: theme.colors.background,
             display: "flex",
             flexDirection: "column",
-            height: "100vh",
-            justifyContent: "center",
-            width: "100vw"
+            height: "30vh",
+            justifyContent: "center"
           }}
         >
           <Heading
@@ -34,7 +37,70 @@ export default class IndexPage extends Component {
             Chris Biscardi
           </Heading>
         </div>
+        <div
+          css={{
+            "@media only screen and (min-width : 700px)": {
+              display: "grid",
+              gridTemplateColumns: "repeat(3, 1fr)",
+              gridGap: ".5rem",
+              margin: "0 1rem"
+            }
+          }}
+        >
+          {featuredPosts.edges.map(({ node }) => (
+            <Link to={node.fields.slug}>
+              <FeaturedPost {...node} />
+            </Link>
+          ))}
+        </div>
       </div>
     );
   }
 }
+
+const FeaturedPost = ({ id, frontmatter, excerpt }) => {
+  if (!frontmatter.featuredImage) {
+    return (
+      <div css={{ background: "white" }}>
+        <h2>{frontmatter.title}</h2>
+        <p>{excerpt}</p>
+      </div>
+    );
+  }
+  return (
+    <Img
+      alt={frontmatter.title}
+      key={id}
+      fluid={frontmatter.featuredImage.childImageSharp.fluid}
+    />
+  );
+};
+
+export const query = graphql`
+  query {
+    featuredPosts: allMdx(
+      limit: 10
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            featuredImage {
+              childImageSharp {
+                fluid(maxWidth: 700) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
