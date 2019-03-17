6ae48b935521ce2534e420ff1d7426a694bfb974
christopherbiscardi <chris@christopherbiscardi.com>
2019-03-17 12:25:15 -0700

    Squashed 'sens8/' changes from ed0dfa7..c0878d0
    c0878d0 font on tags
    
    git-subtree-dir: sens8
    git-subtree-split: c0878d0fb2fb9d3c3d8919a72987a29d519107cf

--- packages/tag/index.js
+++ packages/tag/index.js
@@ -1,41 +1,44 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
-import { useTextColor, useLayers, useLinkColor } from "@sens8/tokens";
+import { useFont, useTextColor, useLayers, useLinkColor } from "@sens8/tokens";
 
 const Tag = props => {
   const textColor = useTextColor();
   const borderColor = useLayers(2);
   const borderColorHover = useLinkColor();
   const backgroundColor = useLayers(0);
+  const fontFamilyStyle = useFont("content");
   return (
     <span
       {...props}
-      css={css`
-        font-size: 14px;
-        font-variant: tabular-nums;
-        line-height: 1.5;
-        color: ${textColor};
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-        list-style: none;
-        display: inline-block;
-        line-height: 20px;
-        height: 22px;
-        padding: 0 7px;
-        border-radius: 4px;
-        border: 1px solid ${borderColor};
-        background: ${backgroundColor};
-        font-size: 12px;
-        transition: all 0.3s cubic-bezier(0.215, 0.61, 0.355, 1);
-        opacity: 1;
-        margin-right: 8px;
-        cursor: pointer;
-        white-space: nowrap;
-        &:hover {
-          border-color: ${borderColorHover};
-        }
-      `}
+      css={[
+        {
+          fontVariant: "tabular-nums",
+          lineHeight: 1.5,
+          color: textColor,
+          boxSizing: "border-box",
+          margin: 0,
+          padding: 0,
+          listStyle: "none",
+          display: "inline-block",
+          lineHeight: "20px",
+          height: "22px",
+          padding: "0 7px",
+          borderRadius: "4px",
+          border: `1px solid ${borderColor}`,
+          background: backgroundColor,
+          fontSize: "12px",
+          transition: "all 0.3s cubic-bezier(0.215, 0.61, 0.355, 1)",
+          opacity: 1,
+          marginRight: "8px",
+          cursor: "pointer",
+          whiteSpace: "nowrap",
+          "&:hover": {
+            borderColor: borderColorHover
+          }
+        },
+        fontFamilyStyle
+      ]}
     />
   );
 };
