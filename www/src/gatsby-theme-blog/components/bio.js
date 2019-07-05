2f2fee10b689c55195feb395821cccddbe1bec97
Christopher Biscardi <chris@christopherbiscardi.com>
2019-07-05 15:28:55 -0700

    update bio

---
+++ www/src/gatsby-theme-blog/components/bio.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import { Styled } from "theme-ui";
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+export default () => (
+  <Fragment>
+    Words by{" "}
+    <Styled.a href="http://twitter.com/chrisbiscardi">Chris Biscardi</Styled.a>.
+  </Fragment>
+);
