30cc214e62a8b8280d1623fd161c280f0f647657
Christopher Biscardi <chris@christopherbiscardi.com>
2019-08-22 15:15:33 -0700

    add libhny

--- go-src/devtips/main.go
+++ go-src/devtips/main.go
@@ -3,9 +3,22 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/honeycombio/libhoney-go"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	// Create an event, add some data
+	ev := libhoney.NewEvent()
+	ev.Add(map[string]interface{}{
+		"method":       request.HTTPMethod,
+		"hostname":     request.Resource,
+		"request_path": request.Path,
+	})
+
+	// This event will be sent regardless of how we exit
+	defer ev.Send()
+
+	ev.AddField("status_code", 200)
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello, World",
@@ -13,6 +26,12 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 }
 
 func main() {
+	libhoney.Init(libhoney.Config{
+		// WriteKey: "",
+		// Dataset:  "",
+	})
+	// Flush any pending calls to Honeycomb before exiting
+	defer libhoney.Close()
 	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(handler)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/christopherbiscardi/christopherbiscardi.github.com
 
 go 1.12
 
-require github.com/aws/aws-lambda-go v1.12.1
+require (
+	github.com/aws/aws-lambda-go v1.12.1
+	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
+	github.com/facebookgo/limitgroup v0.0.0-20150612190941-6abd8d71ec01 // indirect
+	github.com/facebookgo/muster v0.0.0-20150708232844-fd3d7953fd52 // indirect
+	github.com/honeycombio/libhoney-go v1.12.0
+	github.com/klauspost/compress v1.7.6 // indirect
+	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	gopkg.in/alexcesaro/statsd.v2 v2.0.0 // indirect
+)
