b6914a8f23fc2ececc63f1c7659c866e52057eec
Christopher Biscardi <chris@christopherbiscardi.com>
2019-08-28 14:18:03 -0700

    more printer stuff (#107)

--- packages/rainbow-og-images/index.js
+++ packages/rainbow-og-images/index.js
@@ -1,4 +1,6 @@
 const crypto = require("crypto");
+const { store } = require("gatsby/dist/redux");
+const apiRunnerNode = require(`gatsby/dist/utils/api-runner-node`);
 
 const createPrinterNode = ({ id, fileName, renderDir, data, component }) => {
   const fieldData = {
@@ -8,7 +10,8 @@ const createPrinterNode = ({ id, fileName, renderDir, data, component }) => {
     data: JSON.stringify(data),
     component
   };
-  return {
+
+  const node = {
     ...fieldData,
 
     // Required fields.
@@ -23,9 +26,23 @@ const createPrinterNode = ({ id, fileName, renderDir, data, component }) => {
         .digest(`hex`),
       // mediaType: `text/markdown`, // optional
       content: JSON.stringify(fieldData), // optional
-      description: `Printer: node that contains data to be printed into an image` // optional
+      description: `Printer: node that contains data to be printed into an image`, // optional
+      owner: "gatsby-plugin-printer"
     }
   };
+
+  store.dispatch({
+    type: `CREATE_NODE`,
+    payload: node
+  });
+  // we're being sneaky, so we have to called onCreateNode for this new node
+  return apiRunnerNode(`onCreateNode`, {
+    node
+    // traceId,
+    // parentSpan,
+    // traceTags: { nodeId: node.id, nodeType: node.internal.type },
+  });
+  return;
 };
 
 module.exports = {
