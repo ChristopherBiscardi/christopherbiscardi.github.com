db7bf92a762136cc03e8faf70d1013646195f6a2
ChristopherBiscardi <chris@christopherbiscardi.com>
2019-09-21 15:07:22 -0700

    fauna

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -6,7 +6,83 @@ import Layout from "../components/layout";
 import Icon, { iconFromList } from "../components/small-icons";
 import SEO from "../seo";
 import NyanCat from "../components/nyan-cat";
+import { keyframes } from "@emotion/core";
 const maxWidth = "800px";
+
+const Input = ({ label, ...props }) => (
+  <div
+    css={{
+      display: "flex",
+      flexDirection: "column",
+      boxSizing: "border-box"
+    }}
+  >
+    <label css={{ marginBottom: "12px", fontSize: "1rem" }}>{label}</label>
+    <input
+      type="text"
+      css={{
+        boxSizing: "border-box",
+        height: "48px",
+        border: "2px solid #2B3748",
+        borderRadius: "6px",
+        boxShadow: `inset 0 0 8px  rgba(0,0,0,0.1),
+            0 0 16px rgba(0,0,0,0.1)`,
+        padding: `0 8px`,
+        background: `transparent`,
+        margin: `0`,
+        fontSize: "1.5rem",
+        color: "rgba(255,255,255,0.86)"
+      }}
+    />
+  </div>
+);
+
+const gradientAnimation = keyframes`
+  0%{background-position:0% 50%}
+  50%{background-position:100% 50%}
+  100%{background-position:0% 50%}
+`;
+const RainbowBorder = ({ children, ...props }) => (
+  <div
+    css={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      width: "90%",
+      margin: "auto",
+      maxWidth,
+
+      position: "relative",
+      padding: "2rem",
+      boxSizing: "border-box",
+
+      background: "#1b1f2a",
+      backgroundClip: "padding-box",
+      border: "solid 1px transparent",
+      borderRadius: "1rem",
+
+      "& :before": {
+        animation: `${gradientAnimation} 30s ease infinite`,
+        content: '""',
+        position: "absolute",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        zIndex: -1,
+        margin: "-1px",
+        // background: 'linear-gradient(to right, red, orange)',
+        backgroundColor: "#ff1493",
+        background:
+          "linear-gradient(319deg, #ff1493 0%, #0000ff 37%, #ff8c00 100%)",
+
+        borderRadius: "1rem"
+      }
+    }}
+  >
+    {children}
+  </div>
+);
 
 const socialStyles = {
   twitter: { backgroundColor: "#00aced", color: "#eef1f7" },
@@ -200,6 +276,34 @@ export default ({ data, ...props }) => {
           )
         )}
       </List>
+      <RainbowBorder>
+        <h2 css={{ margin: 0, marginBottom: "2rem" }}>Join the Newsletter</h2>
+        <p css={{ margin: 0, marginBottom: "2rem" }}>
+          What do they got in there? King Kong? You're a very talented young
+          man, with your own clever thoughts and ideas. Do you need a manager?
+          Is this my espresso machine? Wh-what is-h-how did you get my espresso
+          machine? Life finds a way.{" "}
+        </p>
+        <div css={{ display: "flex", justifyContent: "space-between" }}>
+          <Input label="Preferred name" />
+          <Input label="Email address" />
+          <button
+            css={{
+              boxSizing: "border-box",
+              height: "48px",
+              width: "140px",
+              borderRadius: "6px",
+              backgroundColor: "#28374A",
+              alignSelf: "flex-end",
+              color: "rgba(255,255,255,0.86)",
+              fontWeight: 600,
+              border: "2px solid #2B3748"
+            }}
+          >
+            Subscribe
+          </button>
+        </div>
+      </RainbowBorder>
     </Layout>
   );
 };
