3f266ac7c424f38e593101022839b8a04dc0d7aa
Jason Lengstorf <jason@lengstorf.com>
2020-03-09 20:27:44 -0700

    fix: update to new config shape

--- netlify-functions/test-streamblitz/test-streamblitz.js
+++ netlify-functions/test-streamblitz/test-streamblitz.js
@@ -5,9 +5,8 @@ exports.handler = createHandler(({ extra: { channel } }) => ({
   message: "yolo",
   description:
     "Every hero needs a catchphrase. Sometimes everyday doofers need them, too.",
-  sfx:
+  audio:
     "https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/lwj-sfx/blitzed.mp3",
-  gif:
-    "https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_400/v1573512575/lwj-sfx/victory",
-  channel
+  image:
+    "https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_400/v1573512575/lwj-sfx/victory"
 }));
