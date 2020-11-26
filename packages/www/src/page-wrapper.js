ed015243fac672bddfba81be7b9bbe3c64d8dbdb
Christopher Biscardi <chris@christopherbiscardi.com>
2020-11-25 20:36:19 -0800

    gradient

--- packages/www/src/page-wrapper.js
+++ packages/www/src/page-wrapper.js
@@ -1117,7 +1117,7 @@ export default ({ children, ...props }) => {
                       ))}
                     </div>
                     <div
-                      className={`flex flex-1 bg-gray-500 ${
+                      className={`flex flex-1 bg-gradient-to-r from-gray-900  to-teal-900 ${
                         !match ? "hidden" : ""
                       }`}
                     >
