ced98e4e8b664c552e768e52d2a4cb1581a7239e
Christopher Biscardi <chris@christopherbiscardi.com>
2020-12-21 01:41:41 -0800

    esinstall

---
+++ packages/www/esinstall.js
@@ -0,0 +1,16 @@
+export const specs = [
+  "preact",
+  "preact/hooks",
+  "@emotion/core",
+  "@mdx-js/preact",
+  "react-helmet",
+  "preact/compat",
+  "@stripe/stripe-js"
+  //   "gsap"
+];
+
+export const options = {
+  alias: {
+    react: "preact/compat"
+  }
+};
